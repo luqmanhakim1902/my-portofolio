0a7c95028d55d660438811fa6365f3310c66edd4
lqmnhkim <144569922+luqmanhakim1902@users.noreply.github.com>
2025-06-17 19:50:05 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Optional: Add a scroll event listener to change header style on scroll
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
